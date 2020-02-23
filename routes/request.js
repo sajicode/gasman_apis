393792013c818be3c16bb05526385bb80508d4a6
sajicode <sajioloye@gmail.com>
2020-02-23 21:27:22 +0100

    expand gas request flow

--- routes/request.js
+++ routes/request.js
@@ -3,17 +3,22 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const { config } = require('dotenv');
 const Distributor = require('../models/Distributor');
+const Transaction = require('../models/Transaction');
 const { authUser } = require('../middleware/auth');
 const geocoder = require('../utils/geocoder');
+const { notifyVendor, cancelRequest, emitRequest } = require('../utils/notifyVendor');
 
 config();
+
+const pricePerKg = process.env.PRICE_PER_KG;
 
 router.post(
 	'/',
 	authUser,
 	[
-		check('address', 'Delivery address is required').not().isEmpty(),
-		check('quantity', 'The quantity of gas is required').notEmpty().isNumeric()
+		check('address', 'Delivery address is required').not().isEmpty().isString(),
+		check('quantity', 'The quantity of gas is required').notEmpty().isNumeric(),
+		check('payment_method', 'Select a payment method').notEmpty().isString()
 	],
 	async (req, res) => {
 		const errors = validationResult(req);
@@ -21,8 +26,8 @@ router.post(
 			return res.status(400).json({ status: 'fail', errors: errors.array() });
 		}
 
-		const { id, fullName } = req.user;
-		const { address, quantity } = req.body;
+		const { id, deviceID } = req.user;
+		const { address, quantity, payment_method } = req.body;
 
 		const location = await geocoder.geocode(address);
 
@@ -45,16 +50,47 @@ router.post(
 			}
 		]);
 
+		//* create transaction
+		const transaction = {
+			paymentMethod: payment_method,
+			customer: id,
+			amount: pricePerKg * quantity,
+			quantity,
+			address
+		};
+
+		const newTransaction = await Transaction.create(transaction);
+
 		if (distributors.length == 0) {
 			return res
 				.status(404)
 				.send({ status: 'fail', message: 'No distributors available in your area at present.' });
 		}
 
+		await notifyVendor(distributors, newTransaction);
+		await cancelRequest(deviceID, distributors.length * 30000);
+
 		res.status(200).send({ status: 'success', data: distributors });
 	}
 
 	// todo - notify distributor, create transaction on acceptance
+);
+
+router.post(
+	'/mimick',
+	[
+		check('distributor_id', 'Distributor ID is required').not().isEmpty().isString(),
+		check('transaction_id', 'Transaction ID is required').notEmpty().isNumeric()
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ status: 'fail', errors: errors.array() });
+		}
+
+		emitRequest('acknowledged', { distributor_id, transaction_id });
+		res.status(200).send({ status: 'success', data: 'Request acknowledgd' });
+	}
 );
 
 module.exports = router;
